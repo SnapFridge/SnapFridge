746b4a7d568830d18ec1ac5a8dad6bb4916f03a7
nicknmi <bluesky10909@yahoo.com>
2025-05-29 11:55:34 -0700

    nav bar for desktop size kind of complete still need styling and mobile version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Button from "../components/Button";
-import Icon from "../components/Icon";
-import ThemeSwitcher from "@components/ThemeSwitcher";
+import NavBar from "@components/NavBar/NavBar";
 
 export default function Page() {
   return (
     <>
-      <ThemeSwitcher></ThemeSwitcher>
+      <NavBar></NavBar>
+      <div>
+        <h1>hshaediqwiejqwe</h1>
+        <div style={{
+          height: "800px",
+        }}>
+          white space
+        </div>
+      </div>
     </>
   );
 }
