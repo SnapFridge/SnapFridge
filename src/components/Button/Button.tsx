58dedfc4f4c44551c965a62ad1832baed09273f7
IReallyWonderWho <81495727+IReallyWonderWho@users.noreply.github.com>
2025-06-15 19:31:16 -0700

    Fix eslint issue with button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -19,7 +19,7 @@ function Button<C extends ElementType = "button">({
   );
 }
 
-const StyledButton = styled("button")({
+const StyledButton = styled("button")<{ styling?: string }>({
   textAlign: "center",
   textDecoration: "none",
   fontSize: `${16 / 16}rem`,
