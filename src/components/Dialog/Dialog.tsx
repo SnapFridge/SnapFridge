09e6d335ea59b03b59964e0a476b2d591b3f90c5
nicknmi <bluesky10909@yahoo.com>
2025-07-04 20:23:23 -0700

    Dialog animations

--- src/components/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import { Dialog } from "radix-ui";
 import Icon from "@components/Icon";
 import Button from "@components/Button";
 import { styled } from "@pigment-css/react";
-import { type PropsWithChildren, type ReactNode } from "react";
+import { useState, type PropsWithChildren, type ReactNode } from "react";
+import { motion, type Variants, AnimatePresence } from "motion/react";
 
 type Props = {
   title: string;
@@ -11,39 +14,101 @@ type Props = {
 };
 
 function AppDialog({ title, description, trigger, children }: PropsWithChildren<Props>) {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <Dialog.Root>
+    <Dialog.Root open={isOpen} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>{trigger}</Dialog.Trigger>
-      <Dialog.Portal>
-        <Overlay />
-        <Content onCloseAutoFocus={(e) => e.preventDefault()}>
-          <Title>{title}</Title>
-          <Description>{description}</Description>
-          {children}
-          <Dialog.Close asChild autoFocus>
-            <XButton variant="icon">
-              <Icon icon="X" description="Close dialog" />
-            </XButton>
-          </Dialog.Close>
-        </Content>
-      </Dialog.Portal>
+      <AnimatePresence>
+        {isOpen && (
+          <Dialog.Portal forceMount>
+            <Overlay>
+              <Background
+                variants={BackgroundVariants}
+                initial="initial"
+                animate="enter"
+                exit="exit"
+              />
+            </Overlay>
+            <ContentContainer>
+              <Content onCloseAutoFocus={(e) => e.preventDefault()} asChild>
+                <motion.div
+                  variants={ContentVariants}
+                  initial="initial"
+                  animate="enter"
+                  exit="exit"
+                >
+                  <Title>{title}</Title>
+                  <Description>{description}</Description>
+                  {children}
+                  <Dialog.Close asChild autoFocus>
+                    <XButton variant="icon">
+                      <Icon icon="X" description="Close dialog" />
+                    </XButton>
+                  </Dialog.Close>
+                </motion.div>
+              </Content>
+            </ContentContainer>
+          </Dialog.Portal>
+        )}
+      </AnimatePresence>
     </Dialog.Root>
   );
 }
 
+const ContentVariants: Variants = {
+  initial: {
+    y: 10,
+    opacity: 0,
+  },
+  enter: {
+    y: 0,
+    opacity: 1,
+  },
+  exit: {
+    y: 10,
+    opacity: 0,
+  },
+};
+
+const BackgroundVariants: Variants = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 0.7,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
+// Set the content to be centered based on a div that takes up the entire screen
+const ContentContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  position: "fixed",
+  top: 0,
+  right: 0,
+  left: 0,
+  bottom: 0,
+});
+
 const Overlay = styled(Dialog.Overlay)({
   // Rare exception where we don't want the color to change
-  background: "#000000",
-  opacity: "70%",
   position: "fixed",
   inset: 0,
 });
 
+const Background = styled(motion.div)({
+  width: "100%",
+  height: "100%",
+  background: "#000000",
+});
+
 const Content = styled(Dialog.Content)({
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
+  position: "relative",
   background: "var(--dialog-background)",
   borderRadius: "16px",
   padding: "12px",
