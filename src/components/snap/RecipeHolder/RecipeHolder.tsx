65e1d9cb70803367f7b9464aed727ba20f7039c2
nicknmi <bluesky10909@yahoo.com>
2025-07-09 02:03:53 -0700

    WIP: Added view more functionality to RecipeHolder

--- src/components/snap/RecipeHolder/RecipeHolder.tsx
+++ src/components/snap/RecipeHolder/RecipeHolder.tsx
@@ -1,13 +1,33 @@
 "use client";
 import RecipeSection from "@components/RecipeSection";
 import { useInputState } from "../InputProvider";
+import Button from "@components/Button";
+import { useState } from "react";
 
 function RecipeHolder() {
   const { state } = useInputState();
+  const [visibleRecipes, setVisibleRecipes] = useState(3);
+
+  function handleViewMore() {
+    let newVisible = visibleRecipes + 3;
+
+    if (newVisible > state.recipes.length) {
+      // TODO: Figure out how to fetch more recipes from spoonacular
+      setVisibleRecipes(state.recipes.length);
+    } else {
+      setVisibleRecipes(newVisible);
+    }
+  }
 
   return (
     <>
-      <RecipeSection recipes={state.recipes} />
+      <div>
+        <RecipeSection recipes={state.recipes.slice(0, visibleRecipes)} />
+        <Button variant="primary" onClick={handleViewMore}>
+          View More
+        </Button>
+      </div>
+
       <RecipeSection headerTxt="Previous Snaps" />
     </>
   );
