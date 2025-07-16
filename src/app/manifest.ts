ab479fdedfcb3bf79741f01a81859d7e3f2ae430
msqr1 <134968689+msqr1@users.noreply.github.com>
2025-07-15 21:34:54 -0700

    Add more metadata

---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import { type MetadataRoute } from "next";
+
+function manifest(): MetadataRoute.Manifest {
+  return {
+    name: "SnapFridge",
+    icons: [
+      {
+        src: "/web-app-manifest-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "/web-app-manifest-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    theme_color: "#ffffff",
+    background_color: "#ffffff",
+    display: "standalone",
+    start_url: "/snap",
+  };
+}
+
+export default manifest;
