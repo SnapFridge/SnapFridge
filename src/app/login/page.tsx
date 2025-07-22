f0972a6611b582e02afe2758c30621973ac7f108
nicknmi <bluesky10909@yahoo.com>
2025-07-22 10:13:08 -0700

    Add sign in functions for Github and Anonymous

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,18 @@
 "use client";
 
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInAnonymously,
+  signInWithPopup,
+  onAuthStateChanged,
+  linkWithPopup,
+} from "firebase/auth";
 import { auth } from "../../utils/firebaseConfig";
+
+interface Provider {
+  provider: "google" | "github";
+}
 
 export default function Page() {
   // todo: maybe move this into its own component, GoogleSignIn.tsx ?
@@ -12,14 +23,73 @@ export default function Page() {
       const res = await signInWithPopup(auth, provider);
       console.log(res.user.displayName);
     } catch (e) {
-      console.log(e);
+      console.error(e);
     }
   }
+
+  async function GithubSignIn() {
+    const provider = new GithubAuthProvider();
+
+    try {
+      const res = await signInWithPopup(auth, provider);
+      console.log(res.user.displayName);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function AnonSignIn() {
+    try {
+      await signInAnonymously(auth);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  async function ConvertAnonToPermanent(provider: Provider) {
+    if (!auth.currentUser) {
+      throw new Error("no auth user found");
+    }
+
+    let authProvider;
+
+    // todo: handle error of auth credential already in use
+    switch (provider.provider) {
+      case "google":
+        authProvider = new GoogleAuthProvider();
+        try {
+          const res = await linkWithPopup(auth.currentUser, authProvider);
+        } catch (e) {
+          throw e;
+        }
+        break;
+
+      case "github":
+        authProvider = new GithubAuthProvider();
+        try {
+          const res = await linkWithPopup(auth.currentUser, authProvider);
+        } catch (e) {
+          throw e;
+        }
+        break;
+    }
+  }
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const uid = user.uid;
+      console.log(uid);
+    } else {
+      console.log("signed out");
+    }
+  });
 
   return (
     <>
       <h1>login page</h1>
       <button onClick={GoogleSignIn}>google sign in</button>
+      <button onClick={GithubSignIn}>github sign in</button>
+      <button onClick={AnonSignIn}>sign in anonymously</button>
     </>
   );
 }
