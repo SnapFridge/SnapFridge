74ae56f92031b7d2f5e482a1079f2b471506d88a
nicknmi <bluesky10909@yahoo.com>
2025-07-21 19:29:41 -0700

    Firebase providers

--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,9 +1,7 @@
-/*
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 interface FirebaseConfig {
   apiKey: string;
@@ -29,4 +27,5 @@ const firebaseConfig: FirebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-*/
+const db = getFirestore(app);
+const auth = getAuth(app);
