8eef2e8f968b0059015f459131ab3c7a27299ff4
IReallyWonderWho <81495727+IReallyWonderWho@users.noreply.github.com>
2025-07-22 23:04:45 -0700

    Redo supabase after merge broke

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,48 +1,46 @@
 "use client";
 
-import {
-  GithubAuthProvider,
-  GoogleAuthProvider,
-  signInAnonymously,
-  onAuthStateChanged,
-  signInWithPopup,
-  linkWithPopup,
-} from "firebase/auth";
-import { auth } from "@utils/firebaseConfig";
 import Button from "@components/Button";
-
-type Provider = GoogleAuthProvider | GithubAuthProvider | "anonymous";
+import { createClient } from "@utils/supabase/component";
 
 export default function Page() {
-  async function signIn(provider: Provider) {
-    if (provider === "anonymous") {
-      await signInAnonymously(auth);
-    } else {
-      await signInWithPopup(auth, provider);
-    }
-  }
-
-  async function linkAnonymous(provider: Exclude<Provider, "anonymous">) {
-    if (!auth.currentUser) {
-      throw new Error("no auth user found");
-    }
-    await linkWithPopup(auth.currentUser, provider);
-  }
-
-  onAuthStateChanged(auth, (usr) => {
-    console.log(usr == null ? "signed out" : usr.displayName);
-  });
+  const supabase = createClient();
 
   return (
     <>
       <h1>Login page</h1>
-      <Button variant={"primary"} onClick={() => signIn(new GoogleAuthProvider())}>
+      <Button
+        variant={"primary"}
+        onClick={() => {
+          supabase.auth.signInWithOAuth({
+            provider: "google",
+            options: {
+              redirectTo: "https://snapfridge.netlify.app/snap",
+            },
+          });
+        }}
+      >
         Google
       </Button>
-      <Button variant={"primary"} onClick={() => signIn(new GithubAuthProvider())}>
+      <Button
+        variant={"primary"}
+        onClick={() => {
+          supabase.auth.signInWithOAuth({
+            provider: "github",
+            options: {
+              redirectTo: "https://snapfridge.netlify.app/snap",
+            },
+          });
+        }}
+      >
         Github
       </Button>
-      <Button variant={"primary"} onClick={() => signIn("anonymous")}>
+      <Button
+        variant={"primary"}
+        onClick={() => {
+          supabase.auth.signInAnonymously();
+        }}
+      >
         Anonymous
       </Button>
     </>
