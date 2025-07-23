e00c105e609e466e83849084c4d5ddee5ff26080
IReallyWonderWho <81495727+IReallyWonderWho@users.noreply.github.com>
2025-07-22 22:17:50 -0700

    This is when the supabase attacked

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,54 +1,12 @@
-"use client";
-
-import {
-  GithubAuthProvider,
-  GoogleAuthProvider,
-  signInAnonymously,
-  signInWithRedirect,
-  linkWithRedirect,
-  getRedirectResult,
-} from "firebase/auth";
-import { auth } from "@utils/firebaseConfig";
 import Button from "@components/Button";
-import { useEffect } from "react";
+import { createClient } from "@utils/supabase/server";
+import Test from "./test";
 
-type Provider = GoogleAuthProvider | GithubAuthProvider | "anonymous";
-
-export default function Page() {
-  async function signIn(provider: Provider) {
-    if (provider === "anonymous") {
-      await signInAnonymously(auth);
-    } else {
-      await signInWithRedirect(auth, provider);
-    }
-  }
-
-  async function linkAnonymous(provider: Exclude<Provider, "anonymous">) {
-    if (!auth.currentUser) {
-      throw new Error("no auth user found");
-    }
-    await linkWithRedirect(auth.currentUser, provider);
-  }
-
-  async function printAuthRes() {
-    const usr = await getRedirectResult(auth);
-    if (usr) {
-      console.log(usr);
-    }
-  }
-  useEffect(() => void printAuthRes(), []);
+export default async function Page() {
   return (
     <>
       <h1>Login page</h1>
-      <Button variant={"primary"} onClick={() => signIn(new GoogleAuthProvider())}>
-        Google
-      </Button>
-      <Button variant={"primary"} onClick={() => signIn(new GithubAuthProvider())}>
-        Github
-      </Button>
-      <Button variant={"primary"} onClick={() => signIn("anonymous")}>
-        Anonymous
-      </Button>
+      <Test />
     </>
   );
 }
