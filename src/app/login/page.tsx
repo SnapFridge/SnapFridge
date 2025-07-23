08077f08e1ee1bb796bc7237e3e2b954c523e59b
nicknmi <bluesky10909@yahoo.com>
2025-07-23 11:43:20 -0700

    OAuth Button wrapper and some styling

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,48 +1,74 @@
 "use client";
 
-import Button from "@components/Button";
 import { createClient } from "@utils/supabase/component";
+import OAuthLoginCards from "@components/OAuthLoginCards";
+import { styled } from "@pigment-css/react";
 
 export default function Page() {
   const supabase = createClient();
 
   return (
     <>
-      <h1>Login page</h1>
-      <Button
-        variant={"primary"}
-        onClick={() => {
-          supabase.auth.signInWithOAuth({
-            provider: "google",
-            options: {
-              redirectTo: "https://snapfridge.netlify.app/snap",
-            },
-          });
-        }}
-      >
-        Google
-      </Button>
-      <Button
-        variant={"primary"}
-        onClick={() => {
-          supabase.auth.signInWithOAuth({
-            provider: "github",
-            options: {
-              redirectTo: "https://snapfridge.netlify.app/snap",
-            },
-          });
-        }}
-      >
-        Github
-      </Button>
-      <Button
-        variant={"primary"}
-        onClick={() => {
-          supabase.auth.signInAnonymously();
-        }}
-      >
-        Anonymous
-      </Button>
+      <Container>
+        <AuthButtonList>
+          <li>
+            <OAuthLoginCards
+              variant="google"
+              onClick={() => {
+                supabase.auth.signInWithOAuth({
+                  provider: "google",
+                  options: {
+                    redirectTo: "https://snapfridge.netlify.app/snap",
+                  },
+                });
+              }}
+            />
+          </li>
+          <li>
+            <OAuthLoginCards
+              variant="github"
+              onClick={() => {
+                supabase.auth.signInWithOAuth({
+                  provider: "github",
+                  options: {
+                    redirectTo: "https://snapfridge.netlify.app/snap",
+                  },
+                });
+              }}
+            />
+          </li>
+          <li>
+            <OAuthLoginCards
+              variant="anon"
+              onClick={() => {
+                supabase.auth.signInAnonymously();
+              }}
+            />
+          </li>
+        </AuthButtonList>
+      </Container>
     </>
   );
 }
+
+const Container = styled("div")({
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+});
+
+const AuthButtonList = styled("ul")({
+  borderRadius: "12px",
+  width: "fit-content",
+  height: "400px",
+  border: "2px solid var(--gray-200)",
+
+  padding: "24px 36px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "18px",
+});
