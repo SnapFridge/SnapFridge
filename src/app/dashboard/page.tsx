cd372fc114b03ed9f456864fabe822c630e22840
nicknmi <bluesky10909@yahoo.com>
2025-07-28 21:56:01 -0700

    Added some text to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,8 @@ import { createClient } from "@utils/supabase/server";
 import LogoutButton from "@components/dashboard/LogoutButton";
 import { PageMargin } from "@utils";
 import DeleteButton from "@components/dashboard/DeleteButton";
+import { styled } from "@pigment-css/react";
+import Counter from "@components/home/Counter";
 
 export default async function Page() {
   const { auth } = await createClient();
@@ -14,11 +16,40 @@ export default async function Page() {
     redirect("/login");
   }
 
+  // todo: update this to work with anonymous
+  let welcomeText: string = user.user_metadata["name"] || user.email;
+
   return (
     <PageMargin>
-      <p>Hello {user.id}</p>
+      <Greeting>
+        Hello, <Name>{welcomeText}</Name>
+      </Greeting>
+      <h2>
+        You currently have <SavedRecipeCounter endValue={727} /> saved recipes!
+      </h2>
       <DeleteButton />
       <LogoutButton />
     </PageMargin>
   );
 }
+
+const Greeting = styled("h1")({});
+
+// thanks gemini
+const Name = styled("span")({
+  backgroundImage: "linear-gradient(to right, var(--text-800), var(--text-400))",
+  WebkitBackgroundClip: "text",
+  backgroundClip: "text",
+  color: "transparent",
+  fontSize: `${36 / 16}rem`,
+  fontWeight: "bold",
+});
+
+// again, thanks gemini
+const SavedRecipeCounter = styled(Counter)({
+  backgroundImage: "linear-gradient(to right, var(--text-900), var(--text-600))",
+  WebkitBackgroundClip: "text",
+  backgroundClip: "text",
+  color: "transparent",
+  fontWeight: "bold",
+});
