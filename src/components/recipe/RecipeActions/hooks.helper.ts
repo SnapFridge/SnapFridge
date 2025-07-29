ac1ad44306419e3184815c5af798cb11116f2f32
IReallyWonderWho <81495727+IReallyWonderWho@users.noreply.github.com>
2025-07-29 16:35:26 -0700

    Saved recipes hook

--- src/components/recipe/RecipeActions/hooks.helper.ts
+++ src/components/recipe/RecipeActions/hooks.helper.ts
@@ -1,18 +1,28 @@
+import useUser from "@components/User";
 import { createClient } from "@utils/supabase/client";
 import { useEffect, useState } from "react";
 
 export function useSavedRecipes() {
-  const [savedRecipes, setSavedRecipes] = useState([]);
+  const [savedRecipes, setSavedRecipes] = useState<number[]>([]);
+  const user = useUser();
 
   useEffect(() => {
     async function getSavedRecipes() {
       const supabase = createClient();
+
+      // Safe to preform as Supabase will not allow multiple rows with the same key (user_id)
+      await supabase.from("saved_recipes").insert({ user_id: user!.id, recipes: [] });
+
+      // Don't need to preform equality checks as supabase should only return the row the user has access to
       const { data } = await supabase.from("saved_recipes").select();
-      console.log(data);
+
+      const recipes: number[] = data ? data[0]?.recipes : [];
+      console.log(recipes);
+      setSavedRecipes(recipes);
     }
 
-    void getSavedRecipes();
-  }, []);
+    if (user) void getSavedRecipes();
+  }, [user]);
 
   return savedRecipes;
 }
