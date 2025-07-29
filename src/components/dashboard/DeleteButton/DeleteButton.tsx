2e472b2c4e888f1375d3437e127ad2da61999f6a
nicknmi <bluesky10909@yahoo.com>
2025-07-28 21:19:30 -0700

    Delete button styling

--- src/components/dashboard/DeleteButton/DeleteButton.tsx
+++ src/components/dashboard/DeleteButton/DeleteButton.tsx
@@ -3,7 +3,8 @@
 import Button from "@components/Button";
 import deleteUser from "./actions";
 import { useState } from "react";
-import { styled } from "@pigment-css/react";
+import { motion, type Variants, AnimatePresence } from "motion/react";
+import "./Button.css";
 
 export default function DeleteButton() {
   const [loading, setLoading] = useState(false);
@@ -30,10 +31,61 @@ export default function DeleteButton() {
 
   return (
     <>
-      <Button onClick={() => handleDeleteUser()} disabled={loading} variant="primary">
+      {/* apparently you can't use :hover with regular js objects so moved style to a vanilla CSS file */}
+      {/* empty style object to override Button's default styling and to prevent errors */}
+      <Button
+        onClick={() => handleDeleteUser()}
+        disabled={loading}
+        variant="primary"
+        className="delete-button"
+        style={{}}
+      >
         {loading ? "Deleting Account..." : "Delete Account"}
       </Button>
-      {error && <p>Error: {error}</p>}
+      <AnimatePresence>
+        {error && (
+          <motion.p
+            style={ErrorMessage}
+            variants={ErrorMessageVariants}
+            initial="initial"
+            animate="enter"
+            exit="exit"
+          >
+            Error: {error}
+          </motion.p>
+        )}
+      </AnimatePresence>
     </>
   );
 }
+
+const ErrorMessageVariants: Variants = {
+  initial: {
+    y: 10,
+    opacity: 0,
+  },
+  enter: {
+    y: 0,
+    opacity: 1,
+  },
+  exit: {
+    opacity: 0,
+  },
+};
+
+const ErrorMessage = {
+  color: "var(--error-700)",
+  fontWeight: "bold",
+};
+
+/*
+const ButtonCSS = {
+  backgroundColor: "transparent",
+  color: "var(--error-400)",
+  border: "2px solid var(--error-400)",
+
+  "&:hover:not(:disabled)": {
+    backgroundColor: "var(--error-100)",
+  },
+};
+*/
