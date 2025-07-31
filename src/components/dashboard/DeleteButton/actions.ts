d6baf59ba471331f629e71c8f56e81d4dc027f77
nicknmi <bluesky10909@yahoo.com>
2025-07-30 17:16:49 -0700

    Added back error throwing in deleteUser

--- src/components/dashboard/DeleteButton/actions.ts
+++ src/components/dashboard/DeleteButton/actions.ts
@@ -4,7 +4,6 @@ import { createClient, createAdminClient } from "@utils/supabase/server";
 import { redirect } from "next/navigation";
 
 async function deleteUser() {
-  console.log("What");
   const { auth: adminAuth } = await createAdminClient();
   const { auth } = await createClient();
 
@@ -14,12 +13,12 @@ async function deleteUser() {
   const { error: deleteError } = await adminAuth.admin.deleteUser(user!.id);
 
   if (deleteError) {
-    console.log("Failed to delete user");
+    throw new Error("Failed to delete user.");
   }
 
   const { error: signOutError } = await adminAuth.signOut();
   if (signOutError) {
-    console.log("Failed to sign out user");
+    throw new Error("Failed to sign out user.");
   }
 
   redirect("/login");
