335c793ac0bd67f2ea8251e6f03aa9857998df83
IReallyWonderWho <81495727+IReallyWonderWho@users.noreply.github.com>
2025-08-03 15:33:59 -0700

    Customize greeting based on time of day

--- src/components/dashboard/Greeting/Greeting.tsx
+++ src/components/dashboard/Greeting/Greeting.tsx
@@ -3,11 +3,25 @@
 import { useUser } from "@components/UserProvider";
 import { styled } from "@pigment-css/react";
 
+function getGreeting() {
+  const date = new Date();
+  // 0-23
+  const timeOfDay = date.getHours();
+  if (timeOfDay < 6 || timeOfDay > 18) {
+    return "evening";
+  } else if (timeOfDay < 12) {
+    return "morning";
+  }
+  return "afternoon";
+}
+
 function Greeting() {
   const user = useUser();
+
   return (
     <h1>
-      Hello, <Name>{user && (user.user_metadata["name"] || user.email)}</Name>
+      Good {getGreeting()},{" "}
+      <Name>{user && (user.user_metadata["name"] || user.email)}</Name>
     </h1>
   );
 }
