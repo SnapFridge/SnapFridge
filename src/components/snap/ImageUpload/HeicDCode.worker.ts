6633f13d750ed5d2f91961066e121b5cd6a390fe
msqr1 <134968689+msqr1@users.noreply.github.com>
2025-08-03 23:55:14 -0700

    ok so build apparently fails with the new changes idk why but let's just ignore for now to deploy

--- src/components/snap/ImageUpload/HeicDCode.worker.ts
+++ src/components/snap/ImageUpload/HeicDCode.worker.ts
@@ -6,15 +6,10 @@ const canvas = new OffscreenCanvas(0, 0);
 const ctx = canvas.getContext("2d")!;
 postMessage("");
 onmessage = async (e) => {
-  // We have to do the as typing for some odd reason otherwise ESLint complains!
-  const [imageDataArray, width, height] = decoder.decode(e.data as Uint8Array) as [
-    Uint8ClampedArray,
-    number,
-    number,
-  ];
-  canvas.width = width;
-  canvas.height = height;
-  ctx.putImageData(new ImageData(imageDataArray, width, height), 0, 0);
+  const out = decoder.decode(e.data as Uint8Array);
+  canvas.width = out[1];
+  canvas.height = out[2];
+  ctx.putImageData(new ImageData(...out), 0, 0);
   const blob = await canvas.convertToBlob();
   postMessage(URL.createObjectURL(blob));
 };
